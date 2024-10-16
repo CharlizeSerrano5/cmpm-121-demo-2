07140bcceb4f39bcaa152c8a3ed3b90f22b5e8f1
Charlize <extrabutterychar@gmail.com>
2024-10-16 05:17:13 -0700

    Completed Step 1 - Initial non-interactive UI layout

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import "./style.css";
 const APP_NAME = "Hi there!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const gameTitle = "Drawing Game!"
+const header = document.createElement("h1");
+header.innerHTML = gameTitle;
+
+const canvas = document.createElement("canvas");
+
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+app.append(header);
+app.append(canvas);
